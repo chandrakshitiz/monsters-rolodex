4feb15c2e8903f03d14db26b5403b7a35458c654
Kshitiz Chandra <kshitiz@Kshitizs-MacBook-Air.local>
2019-12-28 12:44:15 +0530

    Updates

---
+++ precache-manifest.03888ec5e121b92b9aea8c1e2b16d74e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e03a1a203f356f2d4c31f7fdbb9d77c3",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "0b7fba795b942e4475bd",
+    "url": "/monsters-rolodex/static/css/main.2a150bec.chunk.css"
+  },
+  {
+    "revision": "1b43d2eaa999d2a50f77",
+    "url": "/monsters-rolodex/static/js/2.c698f0ee.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.c698f0ee.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0b7fba795b942e4475bd",
+    "url": "/monsters-rolodex/static/js/main.7ea4c478.chunk.js"
+  },
+  {
+    "revision": "366f143aac14b6e58293",
+    "url": "/monsters-rolodex/static/js/runtime-main.1427f30c.js"
+  }
+]);
